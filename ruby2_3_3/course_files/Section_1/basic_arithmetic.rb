204379624a3bf5993bf447ba23d698413cae3ec2
Richard Forth <richard.forth@gmail.com>
2019-11-07 15:41:00 +0000

    Create basic_arithmetic.rb

---
+++ ruby2_3_3/course_files/Section_1/basic_arithmetic.rb
@@ -0,0 +1,29 @@
+# addition
+p 1+4
+#subtraction
+p 10 - 6
+# multiplication
+p 3 * 4
+# string concatenation
+p "School" + "bus"
+# division
+p 10 / 5
+p 12 / 5 # always tries to return an integer
+# solution, make one of the two numbers a float
+p 12.0 / 5
+p 12 / 5.0
+p 12.0 / 5.0
+# exponents
+p 2 * 2 * 2 * 2
+p 2 ** 4
+# modulo (remainder)
+p 5 % 2
+p 6 % 2
+p 13 % 4
+p 14 % 4
+p 15 % 4
+p 16 % 4
+
+# caution  when dealing with floats less tham 1
+#p .5 / 2 # Error
+p 0.5 / 2 # fix: place a zero infront of the decimal point
