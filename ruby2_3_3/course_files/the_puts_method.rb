3e058eb4ce905bc0a13dfed3e2fb3b336223f389
Richard Forth <richard.forth@gmail.com>
2019-11-07 07:18:08 +0000

    Create the_puts_method.rb

---
+++ ruby2_3_3/course_files/the_puts_method.rb
@@ -0,0 +1,27 @@
+puts "Hello World"
+puts "I'm alive and well!"
+
+# can have multiple statements per line
+puts "I'm a statement"; puts "so am  i!"
+
+# printing numbers
+puts "I made 5 dollars at the store"
+puts "I made $64 dollars or a profit of 25%"
+puts 5
+puts 3.14159
+
+puts 4 + 3
+
+# error
+# H:/Documents/GitHub/ruby-tidbits/ruby2_3_3/course_files/the_puts_method.rb:18:in `+': no implicit conversion of Fixnum into String (TypeError)
+#	from H:/Documents/GitHub/ruby-tidbits/ruby2_3_3/course_files/the_puts_method.rb:18:in `<main>'
+#Hello Worldputs "4" + 3
+
+# string math (concatenation)
+puts "4" + "3"
+
+puts
+puts
+puts
+
+puts "See, theres a bunch of line breaks above"
