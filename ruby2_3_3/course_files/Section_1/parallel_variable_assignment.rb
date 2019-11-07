8b8276c44436ae691ebf417c337d7d90baf0d4b4
Richard Forth <richard.forth@gmail.com>
2019-11-07 16:16:43 +0000

    variables

---
+++ ruby2_3_3/course_files/Section_1/parallel_variable_assignment.rb
@@ -0,0 +1,21 @@
+# Paralell Variable Assignment
+a = 10
+b = 20
+c = 30
+
+p a
+p b
+p c
+puts
+
+# shorthand
+p a,b,c
+puts
+puts a,b,c
+puts
+print a,b,c
+puts
+puts
+a,b,c = 10,20,30
+
+p a,b,c
---
+++ ruby2_3_3/course_files/Section_1/variables.rb
@@ -0,0 +1,17 @@
+name = "Boris"
+lastname = "Paskhaver"
+handsome = true
+age = 24 + 1
+
+puts name
+puts lastname
+puts name + lastname
+puts name + " " + lastname
+puts
+puts handsome
+puts
+puts age
+puts age + 5
+puts
+puts "My name is " + name + " " + lastname + "."
+#puts "My name is " + name + " " + lastname + ", and I am " + age + "years old."
