b9d10afda5c57ecda1b0dba55b710a7e99faff95
Richard Forth <richard.forth@gmail.com>
2019-11-07 08:52:28 +0000

    Create the_p_method.rb

---
+++ ruby2_3_3/course_files/Section_1/the_p_method.rb
@@ -0,0 +1,16 @@
+# p offers a more literal ouput
+# shows additional details about the
+# thing we are printing
+
+puts "Steven Seagal"
+p "Steven Seagal"
+
+puts "Hi there, this is a
+big line break"
+
+p "Hi there, this is a
+big line break"
+
+puts 5
+#print 5
+p 5
