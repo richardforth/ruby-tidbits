995a79bdbd947d2dff6b3ee78b8d8543481a123a
Richard Forth <richard.forth@gmail.com>
2019-11-07 08:45:41 +0000

    Create the_print_method.rb

---
+++ ruby2_3_3/course_files/Section_1/the_print_method.rb
@@ -0,0 +1,19 @@
+# the print method is almost identical to puts
+# the main difference is that print does
+# not include a line break automatically
+puts "Hello"
+puts "World"
+
+print "Hello"
+print "World"
+print "blahblahblah"
+
+print "Hello "
+print "world"
+
+print "Hello"
+print " world"
+
+print "Hello"
+print " "
+print "world"
