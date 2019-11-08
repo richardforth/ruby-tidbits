8cfbdcc4a528b7c385d9f9e71c465726d2863dc0
Richard Forth <richard.forth@gmail.com>
2019-11-08 07:01:11 +0000

    added object_methods.rb

---
+++ ruby2_3_3/course_files/Section_1/object_methods.rb
@@ -0,0 +1,23 @@
+# object methods
+p "hello world".length
+p "hello world".upcase
+
+p "HELLO WORLD".downcase
+
+puts
+
+foo = "hello world"
+p foo.length
+p foo.upcase
+
+puts
+
+p 10.next
+p -1.next
+
+puts
+
+puts "hello world"
+puts "hello world".inspect
+
+p "Hello world"
