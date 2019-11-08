e425e6c95dfa736fea028fa198420b0c98a13706
Richard Forth <richard.forth@gmail.com>
2019-11-08 16:35:16 +0000

    updated files

---
+++ ruby2_3_3/course_files/Section_1/variables.rb
@@ -0,0 +1,20 @@
+name = "Boris"
+lastname = "Paskhaver"
+handsome = true
+age = 24 + 1
+
+puts name
+puts lastname
+puts name + lastname
+puts name + " " + lastname
+puts
+puts handsome
+puts
+puts age
+puts age + 5
+puts
+puts "My name is " + name + " " + lastname + "."
+#puts "My name is " + name + " " + lastname + ", and I am " + age + "years old."
+
+# something I learned so I thought I'd come back to this
+puts "My name is #{name} #{lastname} and I am #{age} years old."
