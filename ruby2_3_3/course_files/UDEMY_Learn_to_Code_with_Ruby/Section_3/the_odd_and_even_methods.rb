f9936db4729541e2ac81ad9b7b3a6bef39d96ede
Richard Forth <richard.forth@gmail.com>
2019-11-10 22:33:45 +0000

    added odd and even methods

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_3/the_odd_and_even_methods.rb
@@ -0,0 +1,10 @@
+p 1.next
+p 1.to_s
+
+p 10.odd? # ruby standatd is to use the question mark on any boolean method.
+p 11.odd?
+
+p 2.even?
+p 3.even?
+
+p 2.99.odd? # error (undefined method for float)
