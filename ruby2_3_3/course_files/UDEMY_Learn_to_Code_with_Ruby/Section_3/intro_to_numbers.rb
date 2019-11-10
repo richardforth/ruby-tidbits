218f14c5276481da2949839c56d62d6dbf84c97a
Richard Forth <richard.forth@gmail.com>
2019-11-10 21:51:20 +0000

    added intro to numbers

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_3/intro_to_numbers.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_3/intro_to_numbers.rb
@@ -1,1 +1,11 @@
 # intro to numbers and the .class Method
+p 5.class
+p 0.class
+p 100.class
+p -837.class
+
+p 5.987.class
+p 2.71.class
+p -10.23.class
+
+p 999999999999999999999999999999999999999999999999999999999999999.class
