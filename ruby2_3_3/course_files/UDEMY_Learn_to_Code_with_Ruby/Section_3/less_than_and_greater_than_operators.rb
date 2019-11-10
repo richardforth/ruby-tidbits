c7838d71fa1130d42dd6aa3f01eda3237d444c10
Richard Forth <richard.forth@gmail.com>
2019-11-10 23:11:21 +0000

    less than or greater than

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_3/less_than_and_greater_than_operators.rb
@@ -0,0 +1,26 @@
+# less than (<) or greater than (>) operators return boolean values
+p 1 < 8 # true
+p 10 < 15 # true
+p 12 < 5 # false
+puts
+
+# less than or equal to (<=)
+p 1 <= 5 # true
+p 13 <= 5 # false
+p 5 <=5 # true
+p 5 < 5 # false
+puts
+
+# greater than (>)
+p 10 > 5 # true
+p -1 > -5 # true
+p 12 > 20 # false
+puts
+
+p 12.83 < 21.14
+p 12.83 > 21.14
+puts
+
+p 8 >= 5 # false
+p 8 >= 10 # false
+p 8 >= 8 # true
