cf1593d076666aec630f150016ac324d2ddadd53
Richard Forth <richard.forth@gmail.com>
2019-11-11 19:58:41 +0000

    add object methods and parameters

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_3/object_methods_with_parameters.rb
@@ -0,0 +1,12 @@
+# object methods with parameters (arguments)
+p 3.between?(1,5) # true
+p 6.between?(1,5) # false
+p 'cat'.between?('ant','dog') # true
+p 'gnu'.between?('ant', 'dog') # false
+puts
+
+p 20.between?(10,30)
+p 20.between?(30,50)
+p 1.2.between?(1.1,1.3)
+p -10.5.between?(-20,0)
+p 5.3.between?(8.8,9.6)
