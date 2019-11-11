12aee6767a54f46913c4a3f787e9edeb9cc02a31
Richard Forth <richard.forth@gmail.com>
2019-11-11 19:18:34 +0000

    update arithmetic methods

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_3/arithmetic_methods.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_3/arithmetic_methods.rb
@@ -1,1 +1,23 @@
 # arithmetic_methods
+p 1 + 2
+p 10 - 5
+p 3 * 4
+p 10 / 5
+p 12.0 / 5.0
+p 10 % 3
+puts
+
+p 1.next
+p 1.next() # note paretheses are optional here but allowed
+puts
+
+p 1.+(2) # the plus (+) operator is simply a method, this demostrates it
+p 10.-(5)
+p 10.- 5 # # just to show parenthesis is optional (period).
+p 3.*(4)
+p 10./(5)
+p 12.0./(5.0)
+p 10.div(5) # the div method is the same as /
+p 10.%(3)
+p 10.modulo(3) # the modulo method is the same as %
+puts
