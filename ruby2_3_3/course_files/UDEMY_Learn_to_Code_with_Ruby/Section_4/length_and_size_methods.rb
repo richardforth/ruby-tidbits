94db778c4d4dfb154e890f78657c53823f54417b
Richard Forth <richard.forth@gmail.com>
2019-11-12 14:33:40 +0000

    Create length_and_size_methods.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_4/length_and_size_methods.rb
@@ -0,0 +1,18 @@
+a = "Hello World"
+b = "Hi, my name is Richard!"
+c = " "
+d = ""
+
+p a.length
+p b.length
+p c.length
+p d.length
+puts
+
+p a.size
+p b.size
+p c.size
+p d.size
+puts
+
+# note that length and size are identical
