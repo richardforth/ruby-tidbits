4d5db6c0f25df448232f9287d59d7f56c4ceab84
Richard Forth <richard.forth@gmail.com>
2019-11-12 17:37:23 +0000

    added range method

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_4/extract-multiple_char_from_string_range_object.rb
@@ -0,0 +1,20 @@
+# extracting multiple characters from a string
+# using range objects
+
+story = "Once upon a time in a land far, far away"
+p story[5] # u
+p story[5,4] # upon
+p story.slice(5,4) # upon
+puts
+
+p story[0] # O
+p story[0,4] # Once
+p story.slice(0,4) # Once
+puts
+
+p story[0, story.length]
+p story[10, story.length]
+puts
+
+p story[-7,5]
+p story.slice(-7,5)
