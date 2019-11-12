fa79507547a6ea7024dc19548ee4d39ecfc63541
Richard Forth <richard.forth@gmail.com>
2019-11-12 19:52:12 +0000

    added bang methods

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_4/bang_method_on_strings.rb
@@ -0,0 +1,32 @@
+# bang methods on strings
+word = "hello"
+p word.capitalize
+p word
+puts
+
+word = word.capitalize
+p word
+puts
+
+word = "hello"
+p word
+word.capitalize!
+p word
+puts
+
+
+word.upcase!
+p word
+puts
+
+word.downcase!
+p word
+puts
+
+word.reverse!
+p word
+puts
+
+word.swapcase!
+p word
+puts
