e1616b9e97b55709362a180bb1aa10b39112808a
Richard Forth <richard.forth@gmail.com>
2019-11-12 16:44:39 +0000

    Added brackets method

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_4/extract_single_char_from_string_bracket_method.rb
@@ -0,0 +1,22 @@
+# strings are mutable
+story = "Once upon a time in a land far, far away"
+
+p story.length
+p story[3]
+p story[-1]
+p story[-2]
+p story [-20]
+p story[100]
+
+#slice method
+p story[3]
+p story.slice(3)
+puts
+
+p story[-4]
+p story.slice(-4)
+puts
+
+p story[450]
+p story.slice(450)
+puts
