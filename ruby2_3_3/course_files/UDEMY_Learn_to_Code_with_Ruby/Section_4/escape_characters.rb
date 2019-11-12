bd8ba958850cc83d72176c1f26704e88ec0ce183
Richard Forth <richard.forth@gmail.com>
2019-11-12 08:40:34 +0000

    Create escape_characters.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_4/escape_characters.rb
@@ -0,0 +1,34 @@
+# escape characters
+puts "Hello World"
+puts 'Hello World'
+puts
+
+puts "Juliet said 'Goodbye' to Romeo"
+puts 'Juliet said "Goodbye" to Romeo'
+puts
+
+#puts "Juliet said "Goobye" to Romeo" # error
+puts "Juliet said \"Goodbye\" to Romeo" # works!
+p "Juliet said \"Goodbye\" to Romeo"
+puts
+
+#puts 'Juliet said 'Goobye' to Romeo # error
+puts 'Juliet said \'Goodbye\' to Romeo' # works!
+p 'Juliet said \'Goodbye\' to Romeo'
+puts
+
+# line breaks
+result = "Let's put a line break\nright here"
+puts result
+p result
+puts
+
+# tabs
+puts "\tThis is tab indented"
+puts "This is normal"
+puts
+
+puts "A\tB\tC\tD"
+puts "1\t2\t3\t4"
+puts "5\t6\t7\t8"
+puts
