6c16d29e9f65ccaf338c684384d028092da2ffb3
Richard Forth <richard.forth@gmail.com>
2019-11-12 19:34:22 +0000

    added case methods

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_4/case_methods.rb
@@ -0,0 +1,10 @@
+# case methods
+puts "hello"
+puts "hello".capitalize
+puts "heLLo".capitalize
+puts "Hello".capitalize
+puts "Hello world".capitalize
+puts "Hello World".capitalize
+puts "hello".upcase
+puts "HELLO".downcase
+puts "hElLo".swapcase
