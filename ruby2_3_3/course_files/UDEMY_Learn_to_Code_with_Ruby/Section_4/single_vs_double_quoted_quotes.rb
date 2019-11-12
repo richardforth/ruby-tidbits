5690dc79426611c682fa66c640393a1a2a03908b
Richard Forth <richard.forth@gmail.com>
2019-11-12 09:06:03 +0000

    Create single_vs_double_quoted_quotes.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_4/single_vs_double_quoted_quotes.rb
@@ -0,0 +1,9 @@
+puts "Hello\nWorld"
+# line breaks do not work in single quotes
+puts 'Hello\nWorld'
+puts
+
+phrase = "Hello world"
+puts "#{phrase}"
+# string interpolation does not work in single quotes
+puts '#{phrase}'
