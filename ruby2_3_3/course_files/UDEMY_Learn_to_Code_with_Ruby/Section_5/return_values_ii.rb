4dba471145df7cf382824755d2c73386c964eab6
Richard Forth <richard.forth@gmail.com>
2019-11-14 04:51:05 +0000

    added return values ii

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_5/return_values_ii.rb
@@ -0,0 +1,13 @@
+# return values ii
+
+def nothing
+end
+
+def return_guess
+  puts "Blah blah blah"
+  print "Will return be any different?"
+end
+
+p nothing # nil
+result = return_guess
+p result
