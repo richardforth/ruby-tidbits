295e68657c45ddd992971f0fda9f0405db65b189
Richard Forth <richard.forth@gmail.com>
2019-11-20 11:36:58 +0000

    Create arrays.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_8/arrays.rb
@@ -0,0 +1,23 @@
+# arrays are basically lists
+# we had the same in perl and python, it seemed like the two
+# terms are interchangeable
+keypad = [
+          7,8,9,
+          5,4,6,
+          1,2,3,
+          0]
+
+p keypad
+p keypad.sort
+
+
+myArray = []
+p myArray
+p myArray.class
+
+myArray[0] = "Apple"
+myArray[1] = "Banana"
+
+p myArray
+p myArray[0]
+p myArray[1]
