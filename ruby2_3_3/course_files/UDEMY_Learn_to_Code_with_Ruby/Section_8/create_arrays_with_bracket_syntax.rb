d8746734dbe451809ff7e12eee5a0295a988f444
Richard Forth <richard.forth@gmail.com>
2019-11-21 12:59:59 +0000

    Create create_arrays_with_bracket_syntax.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_8/create_arrays_with_bracket_syntax.rb
@@ -0,0 +1,30 @@
+myArray1 = [] # creates an empty Array
+p myArray1.class # Array
+puts
+
+numbers = [4,8,15,16,23,42] # yes, these are the numbers from LOST, lol :D
+p numbers.class
+p numbers
+print numbers
+puts
+puts numbers
+
+toys = ["Teddy bear", "Super Soaker", "board game"]
+p toys
+print toys
+puts
+puts toys
+
+things = [4, true, "Hello", 10.99]
+
+registrations = [true, true, false, false, true]
+puts registrations
+
+#students = ["Boris", 25, true, "Sally", 23, true]
+students = [["Boris", 25, true],
+            ["Sally", 23, true],
+            ["Ingrid", 31,false]]
+
+puts students
+p students
+print students
