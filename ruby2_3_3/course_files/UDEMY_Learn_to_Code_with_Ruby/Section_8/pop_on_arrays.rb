0af59d455fccbe442eb7d6a73f7da692ef60ff9a
Richard Forth <richard.forth@gmail.com>
2019-11-22 15:22:21 +0000

    Create pop_on_arrays.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_8/pop_on_arrays.rb
@@ -0,0 +1,20 @@
+arr = [1,2,3,4,5,6,7,8,9,10]
+p arr
+
+while arr.length > 0
+  last_item = arr.pop
+  p last_item
+  p arr
+end
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+p arr
+
+two_items = arr.pop(2)
+p two_items
+p arr
+puts
+
+five_items = arr.pop(5)
+p five_items
+p arr
