50f511e5645b1ecc1f4e249b0265c4f3a0b4f37c
Richard Forth <richard.forth@gmail.com>
2019-11-22 15:14:06 +0000

    Create push_shovel_insert.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_8/push_shovel_insert.rb
@@ -0,0 +1,20 @@
+locations = ["House", "Airport", "Bar"]
+p locations
+locations.push("Restaurant")
+p locations
+puts
+
+p locations
+locations.push("Saloon", "Bank")
+p locations
+puts
+
+# shovel operatot
+p locations
+locations << "Sheriff's Office" << "Store"
+p locations
+puts
+
+p locations
+locations.insert(2, "Gunsmith", "Cafe")
+p locations
