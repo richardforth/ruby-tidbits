617ced302c0ece4a927dc1601824bff3cfd3089e
Richard Forth <richard.forth@gmail.com>
2019-11-23 23:29:07 +0000

    Create next.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_9/next.rb
@@ -0,0 +1,13 @@
+def squares(arr)
+  arr.each do |num|
+    if num.is_a?(Fixnum)
+      puts "The square of #{num} is #{num ** 2}"
+    else
+      puts "That's not a number, I'm skipping it!"
+      next
+    end
+  end
+end
+
+numbers = [1,2,3,"Hello",5,6,7]
+squares(numbers)
