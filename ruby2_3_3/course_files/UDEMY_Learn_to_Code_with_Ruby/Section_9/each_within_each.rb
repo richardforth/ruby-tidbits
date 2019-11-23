997fe206752a2a3ee6743cf03b13c97386dedf3e
Richard Forth <richard.forth@gmail.com>
2019-11-23 11:29:01 +0000

    Create each_within_each.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_9/each_within_each.rb
@@ -0,0 +1,8 @@
+shirts = ["striped", "plain white", "plaid", "band"]
+ties = ["polka dot", "solid color", "boring"]
+
+shirts.each do |shirt|
+  ties.each do |tie|
+    puts "Option: A #{shirt} shirt and a #{tie} tie."
+  end
+end
