b249732450ff19419a8bb885b498cfb9fc03dd3e
Richard Forth <richard.forth@gmail.com>
2019-11-23 00:32:51 +0000

    Create each_on_array.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_9/each_on_array.rb
@@ -0,0 +1,19 @@
+candies = ["Sour Patch Kids", "Milky Way", "Airheads"]
+
+candies.each do |candy|
+  puts "I love eating #{candy}"
+  puts "It tastes so good!"
+end
+
+names = ["bo", "moe", "joe"]
+
+names.each do |name|
+  puts name.upcase
+end
+
+numbers = [1, 2, 3, 4, 5]
+
+numbers.each do |num|
+  square = num * num
+  puts "The square of #{num} is #{square}."
+end
