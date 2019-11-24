30b2d61aa49d9518f184e44436e46846a6e4400b
Richard Forth <richard.forth@gmail.com>
2019-11-24 01:56:19 +0000

    max min

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_9/CHALLENGE_max_min.rb
@@ -0,0 +1,12 @@
+def custom_max(arr)
+  # return max value in the array
+end
+
+def custom_min(arr)
+  # return min value in the array
+end
+
+nums = [1,2,3]
+
+custom_max(nums)
+custom_min(nums)
---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_9/max_min_on_arrays.rb
@@ -0,0 +1,9 @@
+stock_prices = [723.99, 434.12, 84.7, 649.92]
+p stock_prices.max # largest value
+p stock_prices.min # smallest value
+puts
+
+fruits = ["apple", "kiwi", "banana", "watermelon"]
+p fruits.max
+p fruits.min
+puts
