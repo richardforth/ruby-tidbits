7462a35be071eb5f6643e1d73ffa243b9adfb2eb
Richard Forth <richard.forth@gmail.com>
2019-11-24 00:33:41 +0000

    Create reverse.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_9/reverse.rb
@@ -0,0 +1,11 @@
+p "Hello".reverse
+p [1, 2, 3].reverse
+p ["A", "B", "C"].reverse
+p [true, true, true, false, true].reverse
+
+# variables
+queue = [4, 8, 15, 16, 23, 42]
+p queue
+
+queue.reverse!
+p queue
