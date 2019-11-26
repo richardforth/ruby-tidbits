1f7c0c34eab2c4c0fb0d0e6eeeae9161efd2ada8
Richard Forth <richard.forth@gmail.com>
2019-11-26 15:25:50 +0000

    Update join_array_returns_string.rb

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_10/join_array_returns_string.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_10/join_array_returns_string.rb
@@ -1,3 +1,7 @@
+# first a quick check of my understanding
 lipsum_words = ["Lorem", "ipsum", "dolor", "sit", "amet"]
 mySentence = lipsum_words.join(" ")
 p mySentence
+# perfect
+
+#=------- and now the lesson begins -------=#
