6f0a156c93563add0c8a9f6de7ab8e884fd56918
Richard Forth <richard.forth@gmail.com>
2019-11-26 12:14:58 +0000

    Update index_and_find_index_on_array.rb

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_9/index_and_find_index_on_array.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_9/index_and_find_index_on_array.rb
@@ -9,3 +9,9 @@ p colors.find_index("Green") # 2
 p colors.find_index("Blue") # 1
 p colors.find_index("Red") # 0 (returns 1st instance only)
 puts
+
+# what if we search for something that does NOT exist in the array?
+# we get nil as a response
+p colors.index("Orange") # nil
+p colors.find_index("Orange") # nil
+puts
