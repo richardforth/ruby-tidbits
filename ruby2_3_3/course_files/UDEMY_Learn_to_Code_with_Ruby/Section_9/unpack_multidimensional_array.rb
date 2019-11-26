215009840effda009112bee11f4af9c0192f945e
Richard Forth <richard.forth@gmail.com>
2019-11-26 12:57:07 +0000

    Create unpack_multidimensional_array.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_9/unpack_multidimensional_array.rb
@@ -0,0 +1,20 @@
+users = [
+  ["Bob", 25, "Male"],
+  ["Susan", 48, "Female"],
+  ["Larry", 32, "Male"]
+]
+p users
+p users[1][2] # Female
+puts
+
+
+bob, susan, larry = users
+p bob
+p susan
+p larry
+puts
+
+p larry[1]
+puts
+
+p susan[2]
