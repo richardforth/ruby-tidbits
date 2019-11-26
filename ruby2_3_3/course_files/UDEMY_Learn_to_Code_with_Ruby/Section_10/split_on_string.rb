11b78d20b757b26cc33b05fd6c49ed4cbf525f46
Richard Forth <richard.forth@gmail.com>
2019-11-26 14:09:55 +0000

    Update split_on_string.rb

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_10/split_on_string.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_10/split_on_string.rb
@@ -4,5 +4,21 @@ ipsum = "Lorem ipsum dolor sit amet"
 lipsum = ipsum.split(" ")
 p lipsum
 # perfect!
+puts
 
 # ..now begins the lesson...
+default = ipsum.split # default delimiter is 'space'
+p default
+lordol = ipsum.split(" ipsum ")
+p lordol
+loramet = ipsum.split(" sit ")
+p loramet
+
+greeting = "Hi, my name is Richard"
+p greeting
+garray = greeting.split
+p garray
+gfact = greeting.split(",")
+p gfact
+msplits = greeting.split("m")
+p msplits
