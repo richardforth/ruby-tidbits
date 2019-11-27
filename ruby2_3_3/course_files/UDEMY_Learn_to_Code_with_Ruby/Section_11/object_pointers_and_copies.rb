6273a4d58e4fb7565fb0e795af3f89661167ce3e
Richard Forth <richard.forth@gmail.com>
2019-11-27 13:14:33 +0000

    Update object_pointers_and_copies.rb

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_11/object_pointers_and_copies.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_11/object_pointers_and_copies.rb
@@ -1,1 +1,39 @@
 # object pointers and object copies
+a = 1
+b = a
+
+a += 1
+p b # b is unchanged
+p a # a now = 2
+p a.object_id
+p b.object_id
+puts
+
+#=----------- lesson begins --------------=#
+a = [1, 2, 3]
+b = [1, 2, 3]
+# these two arrays are completely separate
+p a.object_id
+p b.object_id
+a << 4
+p a
+p b
+puts
+
+a = [1, 2, 3]
+b = a
+p a.object_id
+p b.object_id
+a << 4
+p a
+p b
+puts
+
+a = [1, 2, 3]
+b = a.dup
+p a.object_id
+p b.object_id
+a << 4
+p a
+p b
+puts
