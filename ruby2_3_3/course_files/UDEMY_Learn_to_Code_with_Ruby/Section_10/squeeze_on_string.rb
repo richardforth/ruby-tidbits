3f642793414f49a7d0fbd3d3b3241b588ac3b448
Richard Forth <richard.forth@gmail.com>
2019-11-27 08:44:47 +0000

    Create squeeze_on_string.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_10/squeeze_on_string.rb
@@ -0,0 +1,6 @@
+myString = "Thhhe qquuuicckk bbbrowwn aarrddvarrkkk jummppeddd overrr the lazzzy foxx"
+p myString
+p myString.squeeze
+p myString # not mutated
+p myString.squeeze!("hquckbrwpdkmzx")
+p myString # mutated with bang method
