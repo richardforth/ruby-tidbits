9dcbacfb2a9e0cf8afad94e5d77c3ab7c50a7b51
Richard Forth <richard.forth@gmail.com>
2019-11-28 01:07:12 +0000

    Update ampersand_arrays.rb

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_11/ampersand_arrays.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_11/ampersand_arrays.rb
@@ -9,7 +9,8 @@ arr2 = [3, 4, 5, 6, 7]
 arr3 = arr1 & arr2
 p arr3 # [3, 4, 5]
 
-# we could write this in another way, consider two lovers
+# we could write this in another way, consider two lovers being matched in a
+# "dating app"
 he_likes = ["boxing", "ice cream", "movies", "race cars", "cycling"]
 she_likes = ["knitting", "movies", "cycling", "tennis", "ice cream"]
 
