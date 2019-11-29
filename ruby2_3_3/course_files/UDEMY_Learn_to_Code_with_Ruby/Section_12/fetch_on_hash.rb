25ae359fbe78756f0b6972c81c605b197105a331
Richard Forth <richard.forth@gmail.com>
2019-11-29 09:01:29 +0000

    Create fetch_on_hash.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_12/fetch_on_hash.rb
@@ -0,0 +1,21 @@
+menu = {
+  burger: 3.99,
+  taco: 5.96,
+  chips: 0.5
+}
+
+p menu[:burger]
+p menu[:chips]
+p menu[:salad] # nil
+
+# note .fetch uses parentheses not square brackets
+p menu.fetch(:burger)
+p menu.fetch(:chips)
+# p menu.fetch(:salad) # error
+# On the face of it, .fetch looks less favourable as it does
+# not return a nil value by default, in fact it errors if the key
+# is not found! However you can manually set a default return
+# value in such cases.
+p menu.fetch(:burger, "Not found") # 3.99
+p menu.fetch(:chips, "Not found") # 0.5
+p menu.fetch(:salad, "Not found") # "Not Found"
