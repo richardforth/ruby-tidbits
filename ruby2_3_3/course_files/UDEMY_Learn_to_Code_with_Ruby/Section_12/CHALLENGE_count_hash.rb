d0518fca2f7f3686b97c8ade6c5fdc04afaf0fc4
Richard Forth <richard.forth@gmail.com>
2019-11-30 14:15:42 +0000

    Create CHALLENGE_count_hash.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_12/CHALLENGE_count_hash.rb
@@ -0,0 +1,18 @@
+def value_count(hash, value)
+  count = 0
+  hash.each do |key,val|
+    if value == val
+      count +=1
+    end
+  end
+  count
+end
+
+hash = {
+  a: 5,
+  b: 2,
+  c: 3,
+  d: 5
+}
+
+p value_count(hash, 5)
