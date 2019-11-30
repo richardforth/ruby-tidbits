7d61b6501517498724ed3f43d77ca137da0c4ba1
Richard Forth <richard.forth@gmail.com>
2019-11-30 12:51:00 +0000

    Update each_on_arrays.rb

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_12/each_on_arrays.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_12/each_on_arrays.rb
@@ -16,3 +16,47 @@ capitals.each do |state,capital|
   puts "The capital of #{state} is #{capital}."
 end
 puts
+
+# there is another older methof to be aware of, 'each_pair'
+# it works identically to each:
+capitals.each_pair do |state,capital|
+  puts "The capital of #{state} is #{capital}."
+end
+puts
+
+# Question:
+# WHAT HAPPENES WHEN YOU PNLY SPECIFY ONE BLOCK VARIABLE
+# INSTEAD OF TWO?
+capitals.each do |guess|
+  p guess
+end
+puts
+# THATS RIGHT, YOU'LL GET AN ARRAY...!
+
+# MINI CHALLENGE
+# What do we need to do if we just want the keys, or just
+# the values?
+
+# keys
+capitals.each do |guess|
+  p guess[0]
+end
+puts
+
+# values
+capitals.each do |guess|
+  p guess[1]
+end
+puts
+
+# The alternative method is to still specify the two variables
+# for keys asnd values but just not use one of them, for example:
+capitals.each_pair do |state,capital|
+  puts " - #{capital}."
+end
+puts
+
+capitals.each_pair do |state,capital|
+  puts " + #{state}."
+end
+puts
