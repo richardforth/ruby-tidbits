32d82e33c9189dd1a81352e85b9f715bab1152c2
Richard Forth <richard.forth@gmail.com>
2019-12-05 11:08:46 +0000

    Update retrieve_keys_or_values_from_hash_as_array.rb

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_12/retrieve_keys_or_values_from_hash_as_array.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_12/retrieve_keys_or_values_from_hash_as_array.rb
@@ -7,3 +7,29 @@ shopping_list = {
 
 p shopping_list.keys
 p shopping_list.values
+
+# method chaining to get unique values
+p shopping_list.values.uniq
+
+# note that keys in a hash HAVE to be unique just due to the design
+# of the hash object, so they are guarranteed to be unique:
+
+myhash1 = {a: 1, b: 2, c: 3, a: 1}
+# ruby # WARNING:
+# retrieve_keys_or_values_from_hash_as_array.rb:17:
+#  - warning: key :a is duplicated and overwritten on line 17
+p myhash1 # [{:a=>1, :b=>2, :c=>3}]
+
+# and
+
+myhash2 = {}
+
+myhash2[:a] = 1
+myhash2[:b] = 2
+myhash2[:c] = 3
+myhash2[:a] = 1
+
+p myhash2 # {:a=>1, :b=>2, :c=>3}
+# So you see even if we TRY to duplicate keys, ruby won't let us!
+# henece there is no need to "unique-ify" the keys. Only the grab_values
+# can appear duplicated.
