9b3416e97531cc7ec1861901dac71d1b0d488b10
Richard Forth <richard.forth@gmail.com>
2019-12-05 14:02:58 +0000

    Update array_to_hash.rb

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_13/array_to_hash.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_13/array_to_hash.rb
@@ -1,7 +1,8 @@
 spice_girls = [
   [:scary, "Melanie Brown"],
   [:sporty, "Melanie Chisholm"],
-  [:baby, "Emma Bunton"], [:ginger, "Geri Halliwell"],
+  [:baby, "Emma Bunton"],
+  [:ginger, "Geri Halliwell"],
   [:posh, "Victoria Beckham"]
 ]
 
