d75a274c38fb75a1228a04e257ed7084d6b661bf
Richard Forth <richard.forth@gmail.com>
2019-12-06 13:34:50 +0000

    Create delete_on_hash.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_13/delete_on_hash.rb
@@ -0,0 +1,11 @@
+superheroes = {
+  spiderman: "Peter Parker",
+  superman: "Clark Kent",
+  batman: "Bruce Wayne"
+}
+
+p superheroes
+superheroes.delete(:spiderman)
+p superheroes
+
+# the delete method deletes the key/value pair associated with the key
