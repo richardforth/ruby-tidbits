455d8b0cf24ab60dbf4c0e533eb86aa1856b5d8e
Richard Forth <richard.forth@gmail.com>
2019-12-11 11:31:19 +0000

    Create custom_each.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_14_Blocks_procs_and_lambdas/custom_each.rb
@@ -0,0 +1,24 @@
+def custom_each(array)
+  i = 0
+  while i < array.length
+    yield array[i]
+    i += 1
+  end
+end
+
+names = ["Boris", "Arnold", "Melissa"]
+numbers = [10, 20, 30]
+
+custom_each(names) do |name|
+  puts "The length of #{name} is #{name.length}"
+end
+puts
+
+custom_each(numbers) do |number|
+  puts "The square of #{number} is #{number ** 2}"
+end
+puts
+
+x = [1,2,3]
+# custom_each #  wrong number of arguments (given 0, expected 1) (ArgumentError)
+custom_each(x)
