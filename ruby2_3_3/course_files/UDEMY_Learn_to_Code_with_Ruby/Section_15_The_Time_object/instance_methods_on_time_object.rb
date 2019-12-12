0890910ba293c6017e0955c78564e17c9a65717d
Richard Forth <Richard.Forth@rackspace.co.uk>
2019-12-12 08:53:59 +0000

    Create instance_methods_on_time_object.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_15_The_Time_object/instance_methods_on_time_object.rb
@@ -0,0 +1,19 @@
+today = Time.now
+p today
+puts
+
+#=------- methods ---------=#
+p today.month
+p today.day
+p today.year
+puts
+
+p today.hour
+p today.min
+p today.sec
+puts
+
+p today.wday
+p today.yday
+#p today.yweek # ok got a little excited but sadly nosuchmethod
+p today.yday/7
