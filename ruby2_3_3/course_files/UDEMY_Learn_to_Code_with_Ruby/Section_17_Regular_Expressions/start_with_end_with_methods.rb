287f1248bf3919bba3b23a00b9389ea30cef6b1f
Richard Forth <Richard.Forth@rackspace.co.uk>
2019-12-13 14:55:02 +0000

    Create start_with_end_with_methods.rb

---
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_17_Regular_Expressions/start_with_end_with_methods.rb
@@ -0,0 +1,14 @@
+phrase = "The Ruby Programming Language is amazing!"
+
+puts phrase.start_with?("The Ru")
+puts phrase.end_with?("amazing")
+puts phrase.end_with?("zing!")
+
+def custom_starts_with?(str, subtr)
+  
+end
+
+
+def custom_ends_with?(str, substr)
+
+end
