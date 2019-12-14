5be01fe3227af0c3e7a54ff2710d47a20fb85f39
Richard Forth <Richard.Forth@rackspace.co.uk>
2019-12-14 12:21:00 +0000

    Update start_with_end_with_methods.rb

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_17_Regular_Expressions/start_with_end_with_methods.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_17_Regular_Expressions/start_with_end_with_methods.rb
@@ -1,14 +1,27 @@
+require 'awesome_print'
+
 phrase = "The Ruby Programming Language is amazing!"
 
 puts phrase.start_with?("The Ru")
 puts phrase.end_with?("amazing")
 puts phrase.end_with?("zing!")
 
-def custom_starts_with?(str, subtr)
-  
+def custom_starts_with?(str, substr)
+  # return true if substring is found at the
+  # beginning of the string, false otherwise
+  myStr = str[0, substr.length] == substr
 end
 
 
 def custom_ends_with?(str, substr)
-
+  # return true if substring is found at the
+  # end of the string, false otherwise
+  myStr = str[-substr.length..-1] == substr
 end
+
+puts
+
+p custom_starts_with?(phrase, "The r") # false
+p custom_starts_with?(phrase, "The R") # true
+p custom_ends_with?(phrase, "zing") # false
+p custom_ends_with?(phrase, "amazing!") # true
