e792e52cfc62962066fc9e5f999273719375328d
Richard Forth <richard.forth@gmail.com>
2020-01-02 08:53:08 +0000

    Getters

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_18_Classes_I/Classes/gadget_v006.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_18_Classes_I/Classes/gadget_v006.rb
@@ -8,13 +8,25 @@ class Gadget
   def to_s
     "Gadget #{@production_number} has the username #{@username}, It is made from the #{self.class} class. And it has the ID #{self.object_id}"
   end
+
+  def username
+    @username
+  end
+
+  def production_number
+    @production_number
+  end
 end
 
 phone = Gadget.new
-laptop = Gadget.new
-
-puts phone.to_s
-puts laptop.to_s
 
 # Notes:
-# here we overwrote the to_s method - inherited from Object superclass
+# here we create a getter method
+# remember instance variables are NOT publicly accessible
+# this is where "getters and setters come in"
+
+# some instance variables will be read only -> getters (production_number)
+puts phone.production_number
+# some instance variables will be write only -> setters (password)
+# and some may be read and write enabled -> getters AND setters (username)
+puts phone.username
