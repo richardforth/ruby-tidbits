34cd5a0b60608479c25cff670481f41673063fdd
Richard Forth <richard.forth@gmail.com>
2020-01-07 13:50:05 +0000

    Update mixins_part_ii_extend.rb

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_18_Classes_I/Mixins_II_Custom_Mixins/mixins_part_ii_extend.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_18_Classes_I/Mixins_II_Custom_Mixins/mixins_part_ii_extend.rb
@@ -13,12 +13,21 @@ class Cat
   extend Announcer
 end
 
-watson = Dog.new
-p watson.who_am_i
-puts
+#watson = Dog.new
+#p watson.who_am_i   <----- only works with 'include' or 'prepend' in classes
+#puts
+
+# Note that with extend keyword it is not available on objects, only the class
+# itself.
 
 p Dog.who_am_i
 p Cat.who_am_i
 
-# Note that the extend keyword takes the modules methods and adds them directly
-# to the class, rather than objects created from the class.
+# Note that the extend keyword takes the module's methods and adds them directly
+# to the class, rather than objects created from the class. This means we can
+# call the classes directly.
+
+# Use Cases: Class methods that you do not want to violate the DRY principle:
+# don't
+# repeat
+# yourself. -> DRY
