ea88eee77a57a90cf094769a7acb1721ee747ea4
Richard Forth <Richard.Forth@rackspace.co.uk>
2020-01-08 09:09:43 +0000

    structs

--- ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_20_Classes_II/gadget_v015_structs.rb
+++ ruby2_3_3/course_files/UDEMY_Learn_to_Code_with_Ruby/Section_20_Classes_II/gadget_v015_structs.rb
@@ -1,18 +1,31 @@
+require_relative "app_store"
+
 class Gadget
 
   attr_accessor :username
-  attr_reader :production_number
+  attr_reader :production_number, :apps
 
   def initialize(username, password)
     @username = username
     @password = password if validate_password(password)
     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+    @apps = []
   end
 
   def to_s
     "Gadget #{production_number} has the username #{username}.
     It is made from the #{self.class} class.
     And it has the ID #{object_id}"
+  end
+
+  def install_app(name)
+    app = AppStore.find_app(name)
+    @apps << app unless @apps.include?(app)
+  end
+
+  def delete_app(name)
+    app = apps.find { |installed_app| installed_app.name == name}
+    apps.delete(app) unless app.nil?
   end
 
   def password=(new_password)
@@ -23,14 +36,27 @@ class Gadget
 
   private
 
+  attr_writer :apps
+
   def validate_password(new_password)
     new_password.is_a?(String) && new_password.length >=6 && new_password =~ /\d/
   end
 end
 
 # Notes:
-# Rather than access the instance variable directly,
-# here we access the instance methods (attr_reader, attr_accessor etc).
-# note class is an internal ruby keyword so you have to prefix it with self.
-# The benefits of this (preferring Instance methods) is demonstrated in the
-# separate class "BankAccount" (see bank_account.rb)
+# A struct represents a mini - class with less funtionality that your typical
+# ruby class. A struct is sort of like a class that is only made up of instance
+# variables. To demostrate  this we will set up a module called app_strore
+# (see app_store.rb) which we will fill with structs that represent apps
+# that we can install on our gadget device.
+
+g = Gadget.new("boris", "password")
+p g.apps
+g.install_app(:Chat)
+p g.apps
+g.install_app(:Twitter)
+p g.apps
+g.install_app(:Chat)
+p g.apps
+g.delete_app(:Chat)
+p g.apps
